f97ac27b6e61cdb1e2397fcbfaa85777022d8a01
ogammal <gammal93@mac.com>
2018-10-31 21:36:36 -0400

    app functional but needs styling

--- app.js
+++ app.js
@@ -19,12 +19,16 @@ var buttons = [
     'surfing'
 ];
 
-for (var i = 0; i < buttons.length; i++) {
+function addButtons() {
     var button = $('<button>');
     $('#buttons-display').append(button);
     button.text(buttons[i]);
     button.val(buttons[i]);
     button.addClass('btn');
+}
+
+for (var i = 0; i < buttons.length; i++) {
+    addButtons();
 }
 
 $(document).on("click", ".btn", function() {
@@ -36,7 +40,45 @@ $(document).on("click", ".btn", function() {
     $.ajax({
     url: queryURL,
     method: "GET"
-    }).then(function(response) {
+    })
+    .then(function(response) {
         console.log(queryURL)
+        var results = response.data;
+        for (var i = 0; i < results.length; i++) {
+            var gifDiv = $("<div>");          
+            var rating = results[i].rating;
+            var p = $("<p>").text("Rating: " + rating);
+            var sportImage = $("<img>");
+            sportImage.attr({
+                "src": results[i].images.downsized_still.url, 
+                "data-still": results[i].images.downsized_still.url, 
+                "data-animate": results[i].images.downsized.url,
+                "data-state": "still"
+            }); 
+            sportImage.addClass("img");
+            gifDiv.append(p);
+            gifDiv.append(sportImage);
+            $("#gifs").prepend(gifDiv);
+        };
     });
+});
+
+$("#add-sport").on("click", function(event) {
+    event.preventDefault();
+    var addSport = $("#sport").val().trim();
+    buttons.push(addSport);
+    i = (buttons.length - 1);
+    addButtons();
+});
+
+$(document).on("click", ".img", function() {
+    console.log(this)
+    var state = $(this).attr("data-state")
+    if (state === "still") {
+        $(this).attr("src", $(this).attr("data-animate"));
+        $(this).attr("data-state", "animate");
+    } else {
+        $(this).attr("src", $(this).attr("data-still"));
+        $(this).attr("data-state", "still");
+    }
 });
